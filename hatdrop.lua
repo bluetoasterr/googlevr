8213e7706c3451df83fd8ca0e391819764737c09
bluetoasterr <sebi2790@gmail.com>
2025-09-22 16:50:56 -0400

    Update hatdrop.lua

--- hatdrop.lua
+++ hatdrop.lua
@@ -87,6 +87,17 @@ function Align(Part1,Part0,cf,isflingpart)
             Part1.Velocity = velocity
         end)
         return {SetVelocity = function(self,v) velocity=v end,SetCFrame = function(self,v) cf=v end,}
+    elseif Part0.Name == "moveRH" and parts.right == Part0 then
+        Part1.Anchored = false
+        local con;con=ps:Connect(function()
+            if up~=nil then up=not up end
+            if not Part1:IsDescendantOf(workspace) then con:Disconnect() return end
+            if not _isnetworkowner(Part1) then return end
+            Part1.CanCollide=false
+            Part1.CFrame=Part0.CFrame*cf
+            Part1.AssemblyAngularVelocity = Vector3.new(10,10,10)
+        end)
+        return {SetVelocity = function(self,v) end,SetCFrame = function(self,v) cf=v end,}
     else
         Part1.Anchored = false
         
