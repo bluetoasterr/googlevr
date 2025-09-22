1c41f937aad7a563d5bbaedea323614a18388c07
bluetoasterr <sebi2790@gmail.com>
2025-09-22 17:05:53 -0400

    Update hatdrop.lua

--- hatdrop.lua
+++ hatdrop.lua
@@ -75,8 +75,6 @@ end
 function Align(Part1,Part0,cf,isflingpart) 
     local up = isflingpart
     
-    Part1.Anchored = false
-    
     local attach0 = Instance.new("Attachment")
     attach0.Parent = Part1
     
@@ -87,26 +85,23 @@ function Align(Part1,Part0,cf,isflingpart)
     local alignPos = Instance.new("AlignPosition")
     alignPos.Attachment0 = attach0
     alignPos.Attachment1 = attach1
-    alignPos.MaxForce = (Part0.Name == "moveRH" and parts.right == Part0) and 100000 or 9999999
+    alignPos.MaxForce = 9999999
     alignPos.MaxVelocity = math.huge
-    alignPos.Responsiveness = (Part0.Name == "moveRH" and parts.right == Part0) and 50 or 200
+    alignPos.Responsiveness = 200
     alignPos.Parent = Part1
     
     local alignOri = Instance.new("AlignOrientation")
     alignOri.Attachment0 = attach0
     alignOri.Attachment1 = attach1
-    alignOri.MaxTorque = (Part0.Name == "moveRH" and parts.right == Part0) and 100000 or 9999999
+    alignOri.MaxTorque = 9999999
     alignOri.MaxAngularVelocity = math.huge
-    alignOri.Responsiveness = (Part0.Name == "moveRH" and parts.right == Part0) and 50 or 200
+    alignOri.Responsiveness = 200
     alignOri.Parent = Part1
     
     local con;con=ps:Connect(function()
         if not Part1:IsDescendantOf(workspace) then 
             con:Disconnect() 
             return 
-        end
-        if not _isnetworkowner(Part1) then 
-            Part1:SetNetworkOwner(game.Players.LocalPlayer)
         end
         Part1.CanCollide=false
     end)
