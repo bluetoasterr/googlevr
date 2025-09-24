4d6eac7c7461523c165b6a6dbc78418c2058fc61
bluetoasterr <sebi2790@gmail.com>
2025-09-24 17:09:39 -0400

    Update hatdrop.lua

--- hatdrop.lua
+++ hatdrop.lua
@@ -77,7 +77,7 @@ end
 
 function Align(Part1,Part0,cf,isflingpart) 
     local up = isflingpart
-    local velocity = Vector3.new(20,20,20)
+    local velocity = Vector3.new(0,-30,0)
     local con;con=ps:Connect(function()
         if up~=nil then up=not up end
         if not Part1:IsDescendantOf(workspace) then con:Disconnect() return end
@@ -248,7 +248,7 @@ getgenv().con2 = game:GetService("RunService").RenderStepped:connect(function()
         rightarmalign:SetVelocity(Vector3.new(0,0,-99999999))
         rightarmalign:SetCFrame(CFrame.Angles(math.rad(options.righthandrotoffset.X),math.rad(options.righthandrotoffset.Y),math.rad(options.righthandrotoffset.Z)):Inverse()*CFrame.new(0,0,8*(negitive and -1 or 1)))
     elseif rightarmalign then
-        rightarmalign:SetVelocity(Vector3.new(20,20,20))
+        rightarmalign:SetVelocity(Vector3.new(0,-30,0))
         rightarmalign:SetCFrame(CFrame.new(0,0,0))
     end
 end)
